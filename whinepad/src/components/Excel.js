bfa27eb4882bac41b81c507e5f189857f6b18a6c
joshl26 <joshl26@hotmail.com>
2022-11-10 21:59:46 -0500

    commit

---
+++ whinepad/src/components/Excel.js
@@ -0,0 +1,12 @@
+import react from "react";
+import PropTypes from "prop-types";
+import { useState, useReducer } from "react";
+import clone from "../module/clone";
+
+//do the work
+function Excel({ headers, initialData }) {
+  //same as before
+}
+
+//export
+export default Excel;
