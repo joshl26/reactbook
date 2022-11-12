90c0626155d1895b7e50ce3a014e08ceae4e8500
joshl26 <joshl26@hotmail.com>
2022-11-11 20:59:35 -0500

    commit

--- whinepad/src/components/Discovery.js
+++ whinepad/src/components/Discovery.js
@@ -1,6 +1,8 @@
 import Excel from "./Excel";
 import Logo from "./Logo";
 import Body from "./Body";
+import Button from "./Button";
+import Suggest from "./Suggest";
 
 function Discovery() {
   return (
@@ -13,6 +15,24 @@ function Discovery() {
 
       <h2>Body</h2>
       <Body>I am the content inside the Body</Body>
+
+      <h2>Buttons</h2>
+      <p>
+        Button with onClick:{" "}
+        <Button onClick={() => alert("ouch")}>Click Me</Button>
+      </p>
+      <p>
+        A link: <Button href="https://reactjs.org/">Follow Me</Button>
+      </p>
+      <p>
+        Custom class name:{" "}
+        <Button className="Discovery-custom-button">I do nothing</Button>
+      </p>
+
+      <h2>Suggest</h2>
+      <p>
+        <Suggest options={["eenie", "meenie", "miney", "mo"]} />
+      </p>
     </div>
   );
 }
