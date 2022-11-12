d6ba4e5b2c5186614937a503cddde87a8a920e67
joshl26 <joshl26@hotmail.com>
2022-11-12 00:01:49 -0500

    commit

--- whinepad/src/components/Actions.js
+++ whinepad/src/components/Actions.js
@@ -3,3 +3,28 @@ import "./Actions.css";
 
 import deleteImage from "./../images/close.svg";
 import editImage from "./../images/edit.svg";
+import Button from "./Button";
+
+const Actions = ({ onAction = () => {} }) => (
+  <span className="Actions">
+    <Button
+      className="ActionsInfo"
+      title="More info"
+      onClick={() => onAction("info")}
+    >
+      View Details
+    </Button>
+    <Button title="Edit" onClick={() => onAction("edit")}>
+      <img className="EditIcon" src={editImage} alt="Edit" />
+    </Button>
+    <Button title="Delete" onClick={() => onAction.bind(null, "delete")}>
+      <img className="DeleteIcon" src={deleteImage} alt="Delete" />
+    </Button>
+  </span>
+);
+
+Actions.propTypes = {
+  onAction: PropTypes.func,
+};
+
+export default Actions;
