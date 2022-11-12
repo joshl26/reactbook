30ee46950ae13837e3ec7c1091003db5c075986d
joshl26 <joshl26@hotmail.com>
2022-11-11 20:21:28 -0500

    commit

--- whinepad/src/components/Discovery.js
+++ whinepad/src/components/Discovery.js
@@ -1,16 +1,18 @@
 import Excel from "./Excel";
+import Logo from "./Logo";
+import Body from "./Body";
 
 function Discovery() {
   return (
     <div>
-      <h2>Excel</h2>
-      <Excel
-        headers={["Name", "Year"]}
-        initialData={[
-          ["Charles", "1859"],
-          ["Antoine", "1943"],
-        ]}
-      />
+      <h2>Logo</h2>
+
+      <div style={{ background: "#f6f6f6", display: "inline-block" }}>
+        <Logo />
+      </div>
+
+      <h2>Body</h2>
+      <Body>I am the content inside the Body</Body>
     </div>
   );
 }
